4ea00594abf88a7475a7d47ceca088fb3f25d68d
su-kang <sanguk.kang@cj.net>
2025-09-24 17:31:12 +0900

    eslint prettier install

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <main>
-    <div>
-      <router-view />
-    </div>
-  </main>
+	<main>
+		<div>
+			<router-view />
+		</div>
+	</main>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
 main {
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
+	margin: 0;
+	padding: 0;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
 }
 
 /* 전역 스타일 리셋 */
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 
 body {
-  margin: 0;
-  padding: 0;
+	margin: 0;
+	padding: 0;
 }
 </style>
--- src/utils/localStorageUtil.ts
+++ src/utils/localStorageUtil.ts
@@ -1,41 +1,41 @@
-import dateUtils from "./dateUtils";
+import dateUtils from './dateUtils';
 
 // localStorageUtils.js
 export const localStorageUtils = {
-  setItem: (key: string, value: any) => {
-    try {
-      const serializedValue = JSON.stringify(value);
-      localStorage.setItem(key, serializedValue);
-    } catch (error) {
-      console.error("Error setting item in localStorage", error);
-    }
-  },
+	setItem: (key: string, value: any) => {
+		try {
+			const serializedValue = JSON.stringify(value);
+			localStorage.setItem(key, serializedValue);
+		} catch (error) {
+			console.error('Error setting item in localStorage', error);
+		}
+	},
 
-  getItem: (key: string) => {
-    try {
-      const serializedValue = localStorage.getItem(key);
-      return serializedValue ? JSON.parse(serializedValue) : null;
-    } catch (error) {
-      console.error("Error getting item from localStorage", error);
-      return null;
-    }
-  },
+	getItem: (key: string) => {
+		try {
+			const serializedValue = localStorage.getItem(key);
+			return serializedValue ? JSON.parse(serializedValue) : null;
+		} catch (error) {
+			console.error('Error getting item from localStorage', error);
+			return null;
+		}
+	},
 
-  removeItem: (key: string) => {
-    try {
-      localStorage.removeItem(key);
-    } catch (error) {
-      console.error("Error removing item from localStorage", error);
-    }
-  },
+	removeItem: (key: string) => {
+		try {
+			localStorage.removeItem(key);
+		} catch (error) {
+			console.error('Error removing item from localStorage', error);
+		}
+	},
 
-  clear: () => {
-    try {
-      localStorage.clear();
-    } catch (error) {
-      console.error("Error clearing localStorage", error);
-    }
-  },
+	clear: () => {
+		try {
+			localStorage.clear();
+		} catch (error) {
+			console.error('Error clearing localStorage', error);
+		}
+	},
 };
 
 /**
@@ -43,37 +43,37 @@ export const localStorageUtils = {
  * @param data
  */
 export const onCreate = (data: any) => {
-  try {
-    const localData =
-      localStorageUtils.getItem("data") === null
-        ? []
-        : localStorageUtils.getItem("data");
+	try {
+		const localData =
+			localStorageUtils.getItem('data') === null
+				? []
+				: localStorageUtils.getItem('data');
 
-    let maxId = 0;
-    localData.forEach((element: any) => {
-      if (element?.id > maxId) {
-        maxId = element?.id;
-      }
-    });
-    maxId++;
+		let maxId = 0;
+		localData.forEach((element: any) => {
+			if (element?.id > maxId) {
+				maxId = element?.id;
+			}
+		});
+		maxId++;
 
-    let result: any = [];
-    let chgData = {
-      ...data,
-      // id: localData.length, // 유니크 키
-      id: maxId, // 유니크 키
-      updateDate: dateUtils.getToDay("YYYY-MM-DD HH:mm:ss"), // 날짜
-    };
-    if (localData === null) {
-      result.push(chgData);
-    } else {
-      result = [...localData, chgData];
-    }
-    // 로컬 스토리지 변경
-    localStorageUtils.setItem("data", result);
-  } catch (error) {
-    console.error("Error onCreate localStorage", error);
-  }
+		let result: any = [];
+		const chgData = {
+			...data,
+			// id: localData.length, // 유니크 키
+			id: maxId, // 유니크 키
+			updateDate: dateUtils.getToDay('YYYY-MM-DD HH:mm:ss'), // 날짜
+		};
+		if (localData === null) {
+			result.push(chgData);
+		} else {
+			result = [...localData, chgData];
+		}
+		// 로컬 스토리지 변경
+		localStorageUtils.setItem('data', result);
+	} catch (error) {
+		console.error('Error onCreate localStorage', error);
+	}
 };
 
 /**
@@ -82,26 +82,26 @@ export const onCreate = (data: any) => {
  * @returns
  */
 export const onRead = (search: any) => {
-  try {
-    const localData =
-      localStorageUtils.getItem("data") === null
-        ? []
-        : localStorageUtils.getItem("data");
-    const result = searchByKoreanKeyword(
-      searchByKoreanKeyword(
-        sortArray(localData, "updateDate", search.order),
-        "title",
-        search.title
-      ),
-      "createName",
-      search.createName
-    );
+	try {
+		const localData =
+			localStorageUtils.getItem('data') === null
+				? []
+				: localStorageUtils.getItem('data');
+		const result = searchByKoreanKeyword(
+			searchByKoreanKeyword(
+				sortArray(localData, 'updateDate', search.order),
+				'title',
+				search.title,
+			),
+			'createName',
+			search.createName,
+		);
 
-    return result;
-  } catch (error) {
-    console.error("Error onRead localStorage", error);
-  }
-  return;
+		return result;
+	} catch (error) {
+		console.error('Error onRead localStorage', error);
+	}
+	return;
 };
 
 /**
@@ -110,19 +110,19 @@ export const onRead = (search: any) => {
  * @returns
  */
 export const onReadOne = (id: any) => {
-  try {
-    const localData =
-      localStorageUtils.getItem("data") === null
-        ? []
-        : localStorageUtils.getItem("data");
-    const result = localData.find((item: any) => {
-      return item.id === Number(id);
-    });
-    return result;
-  } catch (error) {
-    console.error("Error onRead localStorage", error);
-  }
-  return;
+	try {
+		const localData =
+			localStorageUtils.getItem('data') === null
+				? []
+				: localStorageUtils.getItem('data');
+		const result = localData.find((item: any) => {
+			return item.id === Number(id);
+		});
+		return result;
+	} catch (error) {
+		console.error('Error onRead localStorage', error);
+	}
+	return;
 };
 
 /**
@@ -131,25 +131,25 @@ export const onReadOne = (id: any) => {
  * @returns
  */
 export const onUpdate = (newData: any) => {
-  try {
-    const localData =
-      localStorageUtils.getItem("data") === null
-        ? []
-        : localStorageUtils.getItem("data");
-    let chgData = {
-      ...newData,
-      updateDate: dateUtils.getToDay("YYYY-MM-DD HH:mm:ss"), // 날짜
-    };
-    const result = localData.map((item: any) =>
-      item.id === chgData.id ? chgData : item
-    );
-    // 로컬 스토리지 변경
-    localStorageUtils.setItem("data", result);
-    return result;
-  } catch (error) {
-    console.error("Error onRead localStorage", error);
-  }
-  return;
+	try {
+		const localData =
+			localStorageUtils.getItem('data') === null
+				? []
+				: localStorageUtils.getItem('data');
+		const chgData = {
+			...newData,
+			updateDate: dateUtils.getToDay('YYYY-MM-DD HH:mm:ss'), // 날짜
+		};
+		const result = localData.map((item: any) =>
+			item.id === chgData.id ? chgData : item,
+		);
+		// 로컬 스토리지 변경
+		localStorageUtils.setItem('data', result);
+		return result;
+	} catch (error) {
+		console.error('Error onRead localStorage', error);
+	}
+	return;
 };
 
 /**
@@ -158,39 +158,38 @@ export const onUpdate = (newData: any) => {
  * @returns
  */
 export const onDelete = (id: any) => {
-  try {
-    const localData =
-      localStorageUtils.getItem("data") === null
-        ? []
-        : localStorageUtils.getItem("data");
-    const result = localData.filter((item: any) => {
-      return item.id !== Number(id);
-    });
-    // 로컬 스토리지 변경
-    localStorageUtils.setItem("data", result);
-    return result;
-  } catch (error) {
-    console.error("Error onRead localStorage", error);
-  }
-  return;
+	try {
+		const localData =
+			localStorageUtils.getItem('data') === null
+				? []
+				: localStorageUtils.getItem('data');
+		const result = localData.filter((item: any) => {
+			return item.id !== Number(id);
+		});
+		// 로컬 스토리지 변경
+		localStorageUtils.setItem('data', result);
+		return result;
+	} catch (error) {
+		console.error('Error onRead localStorage', error);
+	}
+	return;
 };
 
 // 배열 정렬 함수
-function sortArray(arr: any, key: string, order = "asc") {
-  return arr.sort((a: any, b: any) => {
-    if (a[key] < b[key]) {
-      return order === "asc" ? -1 : 1;
-    }
-    if (a[key] > b[key]) {
-      return order === "asc" ? 1 : -1;
-    }
-    return 0;
-  });
+function sortArray(arr: any, key: string, order = 'asc') {
+	return arr.sort((a: any, b: any) => {
+		if (a[key] < b[key]) {
+			return order === 'asc' ? -1 : 1;
+		}
+		if (a[key] > b[key]) {
+			return order === 'asc' ? 1 : -1;
+		}
+		return 0;
+	});
 }
 
 // 한글 검색어가 포함된 객체를 찾는 함수
 function searchByKoreanKeyword(arr: any, key: string, keyword: string) {
-  const regex = new RegExp(keyword, "i"); // 대소문자 구분 없이 검색
-  return arr.filter((item: any) => regex.test(item[key]));
+	const regex = new RegExp(keyword, 'i'); // 대소문자 구분 없이 검색
+	return arr.filter((item: any) => regex.test(item[key]));
 }
-
