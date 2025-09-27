db4d7db86d4c30d5d15bd4573e22e154038b4250
bewisessong <80408986+bewisessong@users.noreply.github.com>
2025-09-27 13:16:42 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
 	<main>
-		<div>
-			<router-view />
-		</div>
+		<router-view />
 	</main>
 </template>
 
@@ -15,24 +13,5 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
-main {
-	margin: 0;
-	padding: 0;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-}
-
-/* 전역 스타일 리셋 */
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-}
-
-body {
-	margin: 0;
-	padding: 0;
-}
-</style>
+<style src="./assets/layout.css"></style>
+<style src="./assets/style.css"></style>
