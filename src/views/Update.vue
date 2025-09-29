46d613568a5ba4c9f1209087a65e013715b32aa7
su-kang <sanguk.kang@cj.net>
2025-09-29 18:02:52 +0900

    update 소스 리펙토링

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -23,7 +23,7 @@
 						v-for="item in ICON_LIST"
 						:key="item.id"
 						class="typeItem"
-						:class="{ selected: item.id === input.typeId }"
+						:class="{ selected: item.id === noticeData.typeId }"
 						@click="onClickItem(item)"
 					>
 						<img :src="item.icon" />
@@ -36,7 +36,12 @@
 				<h4>게시판 정보</h4>
 				<div class="box">
 					<label for="title">제 목</label>
-					<input type="text" id="title" name="title" v-model="input.title" />
+					<input
+						type="text"
+						id="title"
+						name="title"
+						v-model="noticeData.title"
+					/>
 				</div>
 				<div class="box">
 					<label for="createName">작 성 자</label>
@@ -44,7 +49,7 @@
 						type="text"
 						id="createName"
 						name="createName"
-						v-model="input.createName"
+						v-model="noticeData.createName"
 					/>
 				</div>
 			</div>
@@ -54,7 +59,7 @@
 				<textarea
 					id="content"
 					name="content"
-					v-model="input.content"
+					v-model="noticeData.content"
 					placeholder="본문을 입력해주세요"
 				/>
 			</div>
@@ -70,15 +75,14 @@
 <script setup lang="ts">
 import { ICON_LIST } from '@/utils/Constants';
 import { onDelete, onReadOne, onUpdate } from '@/utils/localStorageUtil';
-import { onMounted, reactive, watch } from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const router = useRouter();
 const route = useRoute();
 
 // State
-const noticeData = reactive({});
-const input = reactive({
+const noticeData = reactive({
 	typeId: 1,
 	title: '',
 	createName: '',
@@ -89,13 +93,13 @@ const input = reactive({
 
 // Methods
 const onClickItem = (item: any) => {
-	input.typeId = item.id;
+	noticeData.typeId = item.id;
 };
 
 const onSubmit = () => {
 	// 수정 로직 (localStorage에 저장)
-	onUpdate(input);
-	console.log('수정 데이터:', input);
+	onUpdate(noticeData);
+	console.log('수정 데이터:', noticeData);
 
 	// 수정 후 리스트 페이지로 이동
 	router.push('/');
@@ -113,20 +117,12 @@ const goBack = () => {
 	router.go(-1);
 };
 
-// Watch for noticeData changes
-watch(
-	noticeData,
-	newData => {
-		if (newData && Object.keys(newData).length > 0) {
-			Object.assign(input, newData);
-		}
-	},
-	{ deep: true },
-);
-
 // Lifecycle
 onMounted(() => {
 	const id = route.params.id;
-	Object.assign(noticeData, onReadOne(id));
+	const data = onReadOne(id);
+	if (data) {
+		Object.assign(noticeData, data);
+	}
 });
 </script>
